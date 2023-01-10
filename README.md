9e008ed5bb78fa010aee352451f4f52437bab2fa
Luke Fender <lfender6445@gmail.com>
2023-01-10 07:21:19 -0500

    working transform

---
+++ README.md
@@ -0,0 +1,10 @@
+# codemods
+
+- a collection of example codemods
+
+# setup
+```
+npm install
+npm install -g jscodeshift
+npm run transform
+```
--- transform.js
+++ transform.js
@@ -3,27 +3,31 @@ const jscodeshift = require("jscodeshift");
 const transform = (file, api) => {
   const j = api.jscodeshift;
   // Use jscodeshift to find all calls to enzyme.find
-  const enzymeCalls = j(file.source)
+  const replaceFind = j(file.source)
     .find(j.CallExpression, {
       callee: {
-        // object: { name: "enzyme" },
         property: { name: "find" },
       },
     })
     .forEach((path) => {
-      console.log({path})
-      // Extract the argument passed to enzyme.find
-      const argument = path.node.arguments[0];
-      // Create a new call to the RTL method `getBy`
-      const rtlCall = j.callExpression(
-        j.memberExpression(j.identifier("getBy"), j.identifier("displayName")),
-        [argument]
-      );
-      // Replace the enzyme call with the RTL call
-      j(path).replaceWith(rtlCall);
+      if (path.value.callee.object.name === 'wrapper') {
+        // yay, we found wrapper.find
+        // Extract the argument passed to enzyme.find
+        const argument = path.node.arguments[0];
+        // Create a new call to the RTL method `getByRole`
+        const rtlCall = j.callExpression(
+          j.memberExpression(
+            j.identifier("screen"),
+            j.identifier("getByRole")
+          ),
+          [argument]
+        );
+        // Replace the enzyme call with the RTL call
+        j(path).replaceWith(rtlCall);
+      }
     });
   // Return the modified source code
-  return enzymeCalls.toSource();
+  return replaceFind.toSource();
 };
 
 module.exports = transform;
