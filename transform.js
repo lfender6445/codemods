c67daecbcf3712cb6c3f06fef9bc5737cd066231
Beth Stevic <bethstevic@yahoo.com>
2023-01-17 16:05:01 -0500

    updates to transform file

--- transform.js
+++ transform.js
@@ -1,19 +1,19 @@
 const jscodeshift = require("jscodeshift");
 const utils = require('@codeshift/utils')
 
-import { addRTLImport, replaceFindMethods } from "./transform-helpers";
-
 const transform = (file, api) => {
   const j = api.jscodeshift;
+  const source = j(file.source)
   // Use jscodeshift to find all calls to enzyme.find
-  const replaceFind = j(file.source)
+
+  source
     .find(j.CallExpression, {
       callee: {
         property: { name: "find" },
       },
     })
     .forEach((path) => {
-      if (path.value.callee.object.name === 'wrapper') {
+      if (path.value.callee.object.name === "wrapper") {
         // yay, we found wrapper.find
         // Extract the argument passed to enzyme.find
         const argument = path.node.arguments[0];
@@ -29,8 +29,30 @@ const transform = (file, api) => {
         j(path).replaceWith(rtlCall);
       }
     });
+
+  source.find(j.VariableDeclaration, {
+    kind: "const"
+  }).forEach((path) => {
+    console.log(path.value.declarations)
+    if (path.value.declarations[0].id.name === "wrapper") {
+      j(path).replaceWith(
+        j.identifier("render")
+      )
+    }
+  })
+
+  const addRTLImport = (
+    j.importDeclaration(
+      [j.importDefaultSpecifier(j.identifier('{ getByRole, render, screen }'))],
+      j.stringLiteral('react-testing-library')
+    )
+  )
+
+  // Add RTL import
+  source.get().node.program.body.unshift(addRTLImport)
+
   // Return the modified source code
-  return replaceFind.toSource();
+  return source.toSource();
 };
 
 module.exports = transform;
